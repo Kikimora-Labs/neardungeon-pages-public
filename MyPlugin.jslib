8a2e3a333af1cd600350d7f4d04211c011de5451
Kouprin <alex@koupr.in>
2021-11-28 23:13:56 +0000

    Deploying to master from @ Kikimora-Labs/neardungeon@21d7f01b40363ff86edad90f0b8b932d80bb7895 🚀

--- MyPlugin.jslib
+++
@@ -1,6 +0,0 @@
-
-mergeInto(LibraryManager.library, {
-  SetRotation: function (rot) {
-    dispatchReactUnityEvent("SetRotation", Pointer_stringify(rot));
-  },
-});
---
+++ precache-manifest.8a8b4adfd216df586f9e31bd7bcbd3bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d7209c78fd3b8531062e636a722c534",
+    "url": "/index.html"
+  },
+  {
+    "revision": "974eb19bcea4f9f36c2f",
+    "url": "/static/css/2.fa43bc9e.chunk.css"
+  },
+  {
+    "revision": "f3f300b46fc527f6ae31",
+    "url": "/static/css/main.a96dae51.chunk.css"
+  },
+  {
+    "revision": "974eb19bcea4f9f36c2f",
+    "url": "/static/js/2.7dc67239.chunk.js"
+  },
+  {
+    "revision": "719e0f002bbd20db754fff0ad1d1a154",
+    "url": "/static/js/2.7dc67239.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3f300b46fc527f6ae31",
+    "url": "/static/js/main.d6fdf4bb.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
